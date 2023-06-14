ddf4be232aa6a7868e3d3b26fddbb898abfebc36
Shifna12Zarnaz <shifnazarnaz1205@gmail.com>
2023-06-14 19:41:47 +0530

    Fixed vault unseal issue

--- internal/client/k8s.go
+++ internal/client/k8s.go
@@ -22,6 +22,14 @@ func NewK8SClient(log logging.Logger) (*K8SClient, error) {
 	if err != nil {
 		return nil, err
 	}
+	// var kubeconfig string
+	// if home := homedir.HomeDir(); home != "" {
+	// 	kubeconfig = filepath.Join(home, ".kube", "config")
+	// }
+	// config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
@@ -29,7 +37,6 @@ func NewK8SClient(log logging.Logger) (*K8SClient, error) {
 	}
 	return &K8SClient{client: clientset, log: log}, nil
 }
-
 func (k *K8SClient) CreateOrUpdateSecret(ctx context.Context, secretName, namespace string, data map[string]string) error {
 	secData := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
@@ -38,13 +45,24 @@ func (k *K8SClient) CreateOrUpdateSecret(ctx context.Context, secretName, namesp
 		},
 		StringData: data,
 	}
+	_, err := k.client.CoreV1().Secrets(namespace).Get(context.TODO(), secretName, metav1.GetOptions{})
+	if err != nil && err.Error() == "secrets \""+secretName+"\" not found" {
 
-	createdSecret, err := k.client.CoreV1().Secrets(namespace).Update(context.TODO(), secData, metav1.UpdateOptions{})
-	if err != nil {
-		return errors.WithMessage(err, "error in creating vault secret")
+		createdSecret, err := k.client.CoreV1().Secrets(namespace).Create(context.TODO(), secData, metav1.CreateOptions{})
+		if err != nil {
+			return errors.WithMessage(err, "error in creating vault secret")
+		}
+		k.log.Infof("Secret %s created in namespace %s", createdSecret.Name, createdSecret.Namespace)
+	} else if (err != nil) && err.Error() != "secrets \""+secretName+"\" not found" {
+		return errors.WithMessage(err, "error in getting vault secret")
+	} else {
+		updatedsecret, err := k.client.CoreV1().Secrets(namespace).Update(context.TODO(), secData, metav1.UpdateOptions{})
+		if err != nil {
+			return errors.WithMessage(err, "error in creating vault secret")
+		}
+		k.log.Infof("Secret %s created in namespace %s", updatedsecret.Name, updatedsecret.Namespace)
 	}
 
-	k.log.Infof("Secret %s created in namespace %s", createdSecret.Name, createdSecret.Namespace)
 	return nil
 }
 
@@ -54,8 +72,16 @@ func (k *K8SClient) GetSecret(ctx context.Context, secretName, namespace string)
 		return nil, errors.WithMessage(err, "error in creating vault secret")
 	}
 
+	secretMap := make(map[string]string)
+	for key, value := range secData.Data {
+		val := string(value)
+		secretMap[key] = val
+
+	}
+
 	k.log.Debugf("Secret %s fetched from namespace %s", secretName, namespace)
-	return secData.DeepCopy().StringData, nil
+	return secretMap, nil
+
 }
 
 func (k *K8SClient) GetConfigMapsHasPrefix(ctx context.Context, prefix string) (map[string]map[string]string, error) {
